42c1b47a2969b19edad61ba0b2eaa482229b2fdb
Isaac Dennis <isaacdennissombra@gmail.com>
2023-09-08 23:15:26 -0300

    Add labels to plots

--- cyclistic/4-plot.R
+++ cyclistic/4-plot.R
@@ -7,20 +7,22 @@ bicycles_by_user <- read_csv("result/bicycles-by-user.csv")
 trips_by_month_plot <- ggplot(
     trips_by_month,
     mapping = aes(fill = member_casual, x = ended_at, y = trips)) +
-    geom_bar(position = "dodge", stat = "identity")
+    geom_bar(position = "dodge", stat = "identity") +
+    labs(title = "Number of trips per month by type of customer", x = "Month", y = "Trips")
 
 trips_by_weekday_plot <- ggplot(
     trips_by_weekday,
     mapping = aes(fill = member_casual, x = weekday, y = trips)
 ) +
-    geom_bar(position = "dodge", stat = "identity")
+    geom_bar(position = "dodge", stat = "identity") +
+    labs(title = "Number of trips per weekday by type of customer", x = "Weekday", y = "Trips")
 
 bicycles_by_user_plot <- ggplot(
     bicycles_by_user,
     mapping = aes(fill = rideable_type, x = member_casual, y = trips)
 ) +
     geom_bar(position = "dodge", stat = "identity") +
-    labs(title = "Usage by user and bicycle type", y = "trips", x = "customer type")
+    labs(title = "Usage by user and bicycle type", y = "Trips", x = "Customer type")
 
     
 ggsave("plots/trips-by-month.png", trips_by_month_plot, width = 10, height = 5)
