f2dc2b46c67074639fa830a768d52cc7ba8613fb
Isaac Dennis <isaacdennissombra@gmail.com>
2023-09-09 17:02:20 -0300

    Plot trip_duration_by_weekday

--- cyclistic/4-plot.R
+++ cyclistic/4-plot.R
@@ -3,8 +3,12 @@ library("tidyverse")
 trips_by_month <- read_csv("result/trips-by-month.csv")
 trips_by_weekday <- read_csv("result/trips-by-weekday.csv")
 bicycles_by_user <- read_csv("result/bicycles-by-user.csv")
+trip_duration_by_weekday <- read_csv("result/trip-duration-by-weekday.csv")
 
 trips_by_weekday <- trips_by_weekday %>%
+    mutate(weekday = factor(weekday, levels = c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")))
+
+trip_duration_by_weekday <- trip_duration_by_weekday %>%
     mutate(weekday = factor(weekday, levels = c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")))
 
 trips_by_month_plot <- ggplot(
@@ -20,6 +24,13 @@ trips_by_weekday_plot <- ggplot(
     geom_bar(position = "dodge", stat = "identity") +
     labs(title = "Number of trips per weekday by type of customer", x = "Weekday", y = "Trips")
 
+trip_duration_by_weekday_plot <- ggplot(
+    trip_duration_by_weekday,
+    mapping = aes(fill = member_casual, x = weekday, y = duration)
+) +
+    geom_bar(position = "dodge", stat = "identity") +
+    labs(title = "Median trip duration per weekday by type of customer", x = "Weekday", y = "Duration (min)")
+
 bicycles_by_user_plot <- ggplot(
     bicycles_by_user,
     mapping = aes(fill = rideable_type, x = member_casual, y = trips)
@@ -27,7 +38,9 @@ bicycles_by_user_plot <- ggplot(
     geom_bar(position = "dodge", stat = "identity") +
     labs(title = "Usage by user and bicycle type", y = "Trips", x = "Customer type")
 
-    
+
+
 ggsave("plots/trips-by-month.png", trips_by_month_plot, width = 10, height = 5)
 ggsave("plots/trips-by-weekday.png", trips_by_weekday_plot, width = 10, height = 5)
 ggsave("plots/bicycles-by-user.png", bicycles_by_user_plot, width = 10, height = 5)
+ggsave("plots/trip-duration-by-weekday.png", trip_duration_by_weekday_plot, width = 10, height = 5)
