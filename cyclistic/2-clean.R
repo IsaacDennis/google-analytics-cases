d73f7a25012bd699d4bbc7a1f4eed75d00c0ced4
Isaac Dennis <isaacdennissombra@gmail.com>
2023-09-15 20:55:51 -0300

    Use dataframe functions instead of SQL queries

--- cyclistic/2-clean.R
+++ cyclistic/2-clean.R
@@ -1,14 +1,27 @@
 library("DBI")
 library("RSQLite")
+library("tidyverse")
 
 tripdb <- dbConnect(RSQLite::SQLite(), "tripdata.db")
-dbSendQuery(tripdb, "CREATE TABLE IF NOT EXISTS trips_cleaned AS SELECT * FROM trips")
+tripdf <- dbReadTable(tripdb, "trips")
+
+# Create duration, weekday fields
+tripdf <- tripdf %>% mutate(
+  duration = ended_at - started_at,
+  weekday = wday(
+    as.Date(as.POSIXct(ended_at, tz = "UTC")),
+    label = TRUE,
+    abbr = FALSE,
+    locale = "en_US"
+  )
+)
+
 # Delete trips lasting less than 1 minute (60 seconds)
-affected <- dbExecute(tripdb, "DELETE FROM trips_cleaned WHERE (ended_at - started_at) < 60")
-print(sprintf("%d trips lasting less than 1 minute were deleted.\n", affected))
+tripdf <- tripdf %>% filter(duration >= 60)
 # Delete trips lasting more than 1 day (86400 seconds)
-affected <- dbExecute(tripdb, "DELETE FROM trips_cleaned WHERE (ended_at - started_at) > 86400")
-print(sprintf("%d trips lasting more than 1 day were deleted.\n", affected))
-affected <- dbExecute(tripdb, "DELETE FROM trips_cleaned WHERE start_lat = end_lat AND start_lng = end_lng")
-print(sprintf("%d trips with the same initial and final latitude and longitude were deleted.\n", affected))
+tripdf <- tripdf %>% filter(duration <= 86400)
+# Delete trips with the same initial and final latitude and logitude
+tripdf <- tripdf %>% filter(start_lat != end_lat | start_lng != end_lng)
+
+dbWriteTable(tripdb, "trips_cleaned", tripdf, overwrite = TRUE)
 dbDisconnect(tripdb)
