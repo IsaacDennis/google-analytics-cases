ff2883bd6b045e38117d0208a6af33ca3bdefc7d
Isaac Dennis <isaacdennissombra@gmail.com>
2023-09-15 22:15:12 -0300

    Refactoring

--- cyclistic/3-analysis.R
+++ cyclistic/3-analysis.R
@@ -5,61 +5,38 @@ tripdb <- dbConnect(RSQLite::SQLite(), "tripdata.db")
 tripdf <- dbReadTable(tripdb, "trips_cleaned") %>% as_tibble()
 # Median of trip duration
 statistical_summary <- tripdf %>%
-    mutate(duration = ended_at - started_at) %>%
-    group_by(member_casual) %>%
-    summarise(
-        duration_median = round(median(duration) / 60),
-        duration_mean = round(mean(duration) / 60),
-    )
+  group_by(member_casual) %>%
+  summarise(
+    duration_median = round(median(duration) / 60),
+    duration_mean = round(mean(duration) / 60)
+  )
 
 most_used_end_stations <- tripdf %>%
-    drop_na(end_station_name) %>%
-    count(member_casual, end_station_name, sort = TRUE) %>%
-    group_by(member_casual) %>%
-    slice(1:5)
+  drop_na(end_station_name) %>%
+  count(member_casual, end_station_name, sort = TRUE) %>%
+  group_by(member_casual) %>%
+  slice(1:5)
 
-end_stations_by_month <- tripdf %>%
-    drop_na(end_station_name) %>%
-    mutate(
-        ended_at = format(as.Date(as.POSIXct(ended_at, tz = "UTC")), "%Y-%m")
-    ) %>%
-    count(member_casual, ended_at, end_station_name) %>%
-        arrange(desc(n)) %>%
-        group_by(member_casual, ended_at) %>%
-        slice(1:1)
-    
 trips_by_month <- tripdf %>%
-    select(-started_at) %>%
-    mutate(
-        ended_at = format(as.Date(as.POSIXct(ended_at, tz = "UTC")), "%Y-%m")
-    ) %>%
-    group_by(member_casual, ended_at) %>%
-    summarise(trips = n())
+  select(-started_at) %>%
+  mutate(
+    ended_at = format(as.Date(as.POSIXct(ended_at, tz = "UTC")), "%Y-%m")
+  ) %>%
+  group_by(member_casual, ended_at) %>%
+  summarise(trips = n())
 
 trips_by_weekday <- tripdf %>%
-    mutate(weekday = wday(as.Date(as.POSIXct(ended_at, tz = "UTC")), label = TRUE, abbr = FALSE, locale = "en_US")) %>%
-    group_by(member_casual, weekday) %>%
-    summarise(trips = n())
+  group_by(member_casual, weekday) %>%
+  summarise(trips = n())
 
 trip_duration_by_weekday <- tripdf %>%
-    mutate(
-        duration = ended_at - started_at,
-        weekday = wday(as.Date(as.POSIXct(ended_at, tz = "UTC")), label = TRUE, abbr = FALSE, locale = "en_US")
-    ) %>%
-    group_by(member_casual, weekday) %>%
-        summarise(duration = round(median(duration) / 60, 1))
+  group_by(member_casual, weekday) %>%
+  summarise(duration = round(median(duration) / 60, 1))
 
 bicycles_by_user <- tripdf %>%
-    filter(rideable_type != "docked_bike") %>%
-    group_by(member_casual, rideable_type) %>%
-    summarise(trips = n())
-
-
-print(statistical_summary)
-print(most_used_end_stations)
-print(trips_by_month)
-print(trips_by_weekday)
-print(end_stations_by_month)
+  filter(rideable_type != "docked_bike") %>%
+  group_by(member_casual, rideable_type) %>%
+  summarise(trips = n())
 
 dir.create(file.path("result"))
 write_csv(statistical_summary, "result/statistical-summary.csv")
@@ -68,5 +45,4 @@ write_csv(trips_by_month, "result/trips-by-month.csv")
 write_csv(trips_by_weekday, "result/trips-by-weekday.csv")
 write_csv(trip_duration_by_weekday, "result/trip-duration-by-weekday.csv")
 write_csv(bicycles_by_user, "result/bicycles-by-user.csv")
-write_csv(end_stations_by_month, "result/end-stations-by-month.csv")
 dbDisconnect(tripdb)
